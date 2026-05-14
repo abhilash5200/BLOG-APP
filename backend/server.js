fa3f09f327693d719a8ba015eb165a3693a6d41f
BADDIPALLY ABHILASH REDDY <abhilashreddy5200@gmail.com>
2026-05-14 20:15:37 +0530

    updated-blog-app

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const app = exp();
 
 // ================= CORS =================
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [
+    "https://blog-app-sepia-omega-97.vercel.app",
+    "https://blog-1gl1tcajp-abhilash5200s-projects.vercel.app"
+  ],
   credentials: true
 }));
 
